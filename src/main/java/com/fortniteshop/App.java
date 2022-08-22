0d2069dae379219d5162583430ee05eb214f4cec
NenadGvozdenac <nenadgvozdenacsrb@gmail.com>
2022-08-22 12:44:40 +0200

    Initial Commit

---
+++ src/main/java/com/fortniteshop/App.java
@@ -0,0 +1,13 @@
+package com.fortniteshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
